108340ba775988c33a3cf8eb126b84a879120c87
Jameslo599 <jameslo599@gmail.com>
2021-02-27 01:37:24 -0600

    created empty array 'myLibrary' to hold Book objects generated by user input that is pushed through the function 'addBookToLibrary'.

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+let myLibrary = [];
 
+function Book(title) {
+    //title = prompt("Please enter book title", "");
+    this.title = title;
+    //this.author = author
+}
+
+function addBookToLibrary() {
+    let input = new Book(prompt(""));
+    myLibrary.push(input);
+    return input;
+}
+
+Book.prototype = Object.create(addBookToLibrary.prototype);
