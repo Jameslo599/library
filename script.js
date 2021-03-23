67dd9374c48aaaecfc18e92fe734883baeeb8a8c
Jameslo599 <jameslo599@gmail.com>
2021-03-23 01:21:40 -0500

    working on a modal box which will prompt user to enter information for the new book being added. Also working on placing each book in the array into its own 'card'

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
 let myLibrary = [];
+function updateBooks() {
+for (let i = 0; i < myLibrary.length; i++) {
+    const updatedBooks = myLibrary[i];
+    let json = JSON.stringify(updatedBooks);
+    document.getElementById("bookContainer").innerHTML = json;
+}}
 
 function Book(title) {
-    //title = prompt("Please enter book title", "");
     this.title = title;
-    //this.author = author
 }
 
 function addBookToLibrary() {
     let input = new Book(prompt(""));
     myLibrary.push(input);
+    let json = JSON.stringify(myLibrary);
+    document.getElementById("bookContainer").innerHTML = json;
     return input;
+}
+
+let modal = document.getElementById("modalContainer");
+let button = document.getElementById("newBook");
+let span = document.getElementsByClassName("close")[0];
+button.onclick = function() {
+    modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
 
 Book.prototype = Object.create(addBookToLibrary.prototype);
