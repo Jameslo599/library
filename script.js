05897343a964a7c958de32d85ef8cbf01c9de7c8
Jameslo599 <jameslo599@gmail.com>
2021-03-24 02:02:36 -0500

    wrote a for loop to generate a book 'card' every time a new book is submitted. Book information is now entered via modal popup. Upon submitting information, a new Book object is created and stored in the myLibrary array.

--- script.js
+++ script.js
@@ -6,16 +6,10 @@ for (let i = 0; i < myLibrary.length; i++) {
     document.getElementById("bookContainer").innerHTML = json;
 }}
 
-function Book(title) {
+function Book(author, title, pages) {
+    this.author = author;
     this.title = title;
-}
-
-function addBookToLibrary() {
-    let input = new Book(prompt(""));
-    myLibrary.push(input);
-    let json = JSON.stringify(myLibrary);
-    document.getElementById("bookContainer").innerHTML = json;
-    return input;
+    this.pages = pages;
 }
 
 let modal = document.getElementById("modalContainer");
@@ -32,5 +26,47 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+let form = document.getElementById("form");
 
-Book.prototype = Object.create(addBookToLibrary.prototype);
+function submitForm(event) {
+    event.preventDefault();
+    let author = document.getElementById("author").value;
+    let title = document.getElementById("title").value;
+    let pages = document.getElementById("pages").value;
+    let submission = new Book(author, title, pages);
+    myLibrary.push(submission);
+    showHeroes();
+}
+
+function resetForm(event) {
+    event.preventDefault();
+  document.getElementById("author").value = "";
+  document.getElementById("title").value = "";
+  document.getElementById("pages").value = "";
+}
+
+form.addEventListener('submit', submitForm);
+form.addEventListener('submit', resetForm);
+
+function showHeroes() {
+    for (let i = (0 + (myLibrary.length - 1)); i < myLibrary.length; i++) {
+        let section = document.getElementById("bookHolder");
+      const myArticle = document.createElement('article');
+      const myH2 = document.createElement('h2');
+      const myPara1 = document.createElement('p');
+      const myPara2 = document.createElement('p');
+      //const myPara3 = document.createElement('p');
+  
+      myH2.textContent = myLibrary[i].author;
+      myPara1.textContent = 'Secret identity: ' + myLibrary[i].title;
+      myPara2.textContent = 'Age: ' + myLibrary[i].pages;
+      //myPara3.textContent = 'Superpowers:';
+
+      myArticle.appendChild(myH2);
+      myArticle.appendChild(myPara1);
+        myArticle.appendChild(myPara2);
+        //myArticle.appendChild(myPara3);
+
+        section.appendChild(myArticle);
+  }
+}
